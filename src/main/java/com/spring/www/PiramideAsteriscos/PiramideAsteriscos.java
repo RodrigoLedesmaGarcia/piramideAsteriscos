7e7c1aa0c3de3363815ce010d4b2ff7e0a86be39
RodrigoLedesmaGarcia <rodrigo_ledesma_garcia@outlook.com>
2025-05-18 15:02:38 -0600

    first commit

---
+++ src/main/java/com/spring/www/PiramideAsteriscos/PiramideAsteriscos.java
@@ -0,0 +1,31 @@
+package com.spring.www.PiramideAsteriscos;
+
+import java.util.Random;
+
+public class PiramideAsteriscos {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int altura = random.nextInt(21);
+
+        System.out.println(" ");
+        if (altura < 5){
+            altura += 6;
+            piramide(altura);
+        } else {
+            piramide(altura);
+        }
+        System.out.println(" ");
+    }
+    public static void piramide(int altura){
+        for (int i = 1; i <= altura; i++){
+            for (int k = 1; k <= altura - i; k++){
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}

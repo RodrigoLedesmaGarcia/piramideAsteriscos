93de61abe907373b686a8b31435bb27e4bde4e6d
RodrigoLedesmaGarcia <rodrigo_ledesma_garcia@outlook.com>
2025-05-18 15:24:08 -0600

    second commit

--- src/main/java/com/spring/www/PiramideAsteriscos/PiramideAsteriscos.java
+++ src/main/java/com/spring/www/PiramideAsteriscos/PiramideAsteriscos.java
@@ -25,7 +25,7 @@ public class PiramideAsteriscos {
             for (int j = 1; j <= i; j++){
                 System.out.print("* ");
             }
-            System.out.println(" ");
+            System.out.println(" "+i);
         }
     }
 }
